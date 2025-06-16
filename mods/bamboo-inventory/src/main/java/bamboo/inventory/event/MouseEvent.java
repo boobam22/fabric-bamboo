fa360740915a68e5d8ea1dad12e3bfbcebbb1ee1
nia <mangrove0720@outlook.com>
2025-06-17 04:03:30 +0800

    base action

--- mods/bamboo-inventory/src/main/java/bamboo/inventory/event/MouseEvent.java
+++ mods/bamboo-inventory/src/main/java/bamboo/inventory/event/MouseEvent.java
@@ -1,13 +1,51 @@
 package bamboo.inventory.event;
 
-public class MouseEvent {
+import org.lwjgl.glfw.GLFW;
+
+import net.minecraft.screen.slot.Slot;
+
+import bamboo.inventory.action.BaseActionInterface;
+
+public class MouseEvent implements BaseActionInterface {
     public static final MouseEvent INSTANCE = new MouseEvent();
 
     private MouseEvent() {
     }
 
+    private boolean shouldCancel() {
+        Slot slot = getFocusedSlot();
+        return slot == null || !slot.hasStack();
+    }
+
+    private boolean isKeyPressed(long window, int key) {
+        return GLFW.glfwGetKey(window, key) != GLFW.GLFW_RELEASE;
+    }
+
     public boolean handleClick(long window, int button, int action, int modifiers) {
-        return false;
+        if (shouldCancel() || action != GLFW.GLFW_PRESS) {
+            return false;
+        }
+
+        Slot slot = getFocusedSlot();
+        if (isKeyPressed(window, GLFW.GLFW_KEY_M)) {
+            leftClick(slot);
+        } else if (isKeyPressed(window, GLFW.GLFW_KEY_N)) {
+            rightClick(slot);
+        } else if (isKeyPressed(window, GLFW.GLFW_KEY_B)) {
+            shiftClick(slot);
+        } else if (isKeyPressed(window, GLFW.GLFW_KEY_V)) {
+            dropOne(slot);
+        } else if (isKeyPressed(window, GLFW.GLFW_KEY_C)) {
+            dropAll(slot);
+        } else if (isKeyPressed(window, GLFW.GLFW_KEY_L)) {
+            leftClickOutside();
+        } else if (isKeyPressed(window, GLFW.GLFW_KEY_K)) {
+            rightClickOutside();
+        } else {
+            return false;
+        }
+
+        return true;
     }
 
     public boolean handleScroll(long window, double horizontal, double vertical) {
