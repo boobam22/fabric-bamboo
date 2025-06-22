8f94a72718f2a5d80ec18f9dfbfd3b2cb0e7a4a6
nia <mangrove0720@outlook.com>
2025-06-22 18:06:39 +0800

    implement toggle

--- mods/bamboo-camera/src/main/java/bamboo/camera/Camera.java
+++ mods/bamboo-camera/src/main/java/bamboo/camera/Camera.java
@@ -1,11 +1,43 @@
 package bamboo.camera;
 
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.Entity.RemovalReason;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.MarkerEntity;
+
 public class Camera {
     public static final Camera INSTANCE = new Camera();
+    private boolean originChunkCullingEnabled;
+    private Entity originCameraEntity;
+    private MarkerEntity cameraEntity;
 
     private Camera() {
     }
 
     public void toggle() {
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (isActive()) {
+            client.setCameraEntity(originCameraEntity);
+            client.chunkCullingEnabled = originChunkCullingEnabled;
+
+            originCameraEntity = null;
+            cameraEntity.remove(RemovalReason.KILLED);
+        } else {
+            originCameraEntity = client.getCameraEntity();
+            originChunkCullingEnabled = client.chunkCullingEnabled;
+
+            cameraEntity = new MarkerEntity(EntityType.MARKER, client.player.clientWorld);
+            cameraEntity.setPosition(originCameraEntity.getEyePos());
+            cameraEntity.setAngles(originCameraEntity.getYaw(), originCameraEntity.getPitch());
+            client.player.clientWorld.addEntity(cameraEntity);
+
+            client.setCameraEntity(cameraEntity);
+            client.chunkCullingEnabled = false;
+        }
+    }
+
+    public boolean isActive() {
+        return originCameraEntity != null;
     }
 }
