7511788ba074e8608b23285b4ce98a3d340a2994
nia <mangrove0720@outlook.com>
2025-06-23 08:01:28 +0800

    cancel render hand

--- mods/bamboo-camera/src/main/java/bamboo/camera/mixin/GameRendererMixin.java
+++ mods/bamboo-camera/src/main/java/bamboo/camera/mixin/GameRendererMixin.java
@@ -2,7 +2,9 @@ package bamboo.camera.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.MinecraftClient;
@@ -18,5 +20,12 @@ public abstract class GameRendererMixin {
             return MinecraftClient.getInstance().player;
         }
         return entity;
+    }
+
+    @Inject(method = "renderHand", at = @At("HEAD"), cancellable = true)
+    private void renderHand(CallbackInfo ci) {
+        if (Camera.isActive()) {
+            ci.cancel();
+        }
     }
 }
