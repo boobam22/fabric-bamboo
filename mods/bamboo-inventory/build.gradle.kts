3211c35801a97b09c82014cfbefed9363167483c
nia <mangrove0720@outlook.com>
2025-06-24 03:21:10 +0800

    bamboo-inventory-0.3.0

--- mods/bamboo-inventory/build.gradle.kts
+++ mods/bamboo-inventory/build.gradle.kts
@@ -1,3 +1,19 @@
+plugins {
+    id("com.gradleup.shadow")
+}
+
 dependencies {
     implementation("com.belerweb:pinyin4j:2.5.0")
+}
+
+tasks.shadowJar {
+    dependencies {
+        include(dependency("com.belerweb:pinyin4j:2.5.0"))
+    }
+    relocate("net.sourceforge", "bamboo.inventory.shadow")
+}
+
+tasks.remapJar {
+    dependsOn(tasks.shadowJar)
+    input.set(tasks.shadowJar.get().archiveFile)
 }
