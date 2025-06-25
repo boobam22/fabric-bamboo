d490dfa5d1cb43e26c25e64923e1e7d60011e67e
nia <mangrove0720@outlook.com>
2025-06-25 09:00:16 +0800

    add KeyBinding

---
+++ mods/bamboo-lib/src/main/java/bamboo/lib/keybinding/KeyBinding.java
@@ -0,0 +1,72 @@
+package bamboo.lib.keybinding;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+import java.util.function.Consumer;
+
+import org.lwjgl.glfw.GLFW;
+
+import net.minecraft.client.MinecraftClient;
+
+public class KeyBinding {
+    private int key;
+    private List<Integer> modifier;
+    private int action;
+
+    public KeyBinding(int key, List<Integer> modifier, int action) {
+        this.key = key;
+        this.modifier = modifier;
+        this.action = action;
+    }
+
+    public KeyBinding(int key, List<Integer> modifier) {
+        this(key, modifier, GLFW.GLFW_PRESS);
+    }
+
+    public KeyBinding triggerOnPress() {
+        this.action = GLFW.GLFW_PRESS;
+        return this;
+    }
+
+    public KeyBinding triggerOnRelease() {
+        this.action = GLFW.GLFW_RELEASE;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof KeyBinding other) {
+            return key == other.key && modifier.equals(other.modifier) && action == other.action;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(key, modifier, action);
+    }
+
+    @Override
+    public String toString() {
+        return Stream.concat(modifier.stream(), Stream.of(key))
+                .map(KeyMap::toName)
+                .collect(Collectors.joining("+"));
+    }
+
+    public static KeyBinding bind(String key) {
+        List<Integer> keyCodes = Stream.of(key.split("\\+"))
+                .map(KeyMap::toCode)
+                .filter(code -> code != -1)
+                .toList();
+
+        if (keyCodes.size() > 0) {
+            return new KeyBinding(keyCodes.getLast(), keyCodes.subList(0, keyCodes.size() - 1));
+        }
+        return null;
+    }
+
+    public static void execute(Consumer<MinecraftClient> callback) {
+    }
+}
