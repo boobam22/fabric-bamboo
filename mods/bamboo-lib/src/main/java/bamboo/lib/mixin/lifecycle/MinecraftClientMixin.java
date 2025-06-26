18b96b9052a5ea13b3bf4275f22f0afffe003fd4
nia <mangrove0720@outlook.com>
2025-06-27 07:25:50 +0800

    register handler

--- mods/bamboo-lib/src/main/java/bamboo/lib/mixin/lifecycle/MinecraftClientMixin.java
+++ mods/bamboo-lib/src/main/java/bamboo/lib/mixin/lifecycle/MinecraftClientMixin.java
@@ -9,20 +9,22 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.client.MinecraftClient;
 
+import bamboo.lib.lifecycle.MinecraftClientLifecycle;
+
 @Mixin(MinecraftClient.class)
 public abstract class MinecraftClientMixin {
     @Inject(method = "joinWorld", at = @At("TAIL"))
     private void joinWorld(CallbackInfo ci) {
-        System.out.println("joinWorld");
+        MinecraftClientLifecycle.onJoinWorld();
     }
 
     @Inject(method = "disconnect(Lnet/minecraft/client/gui/screen/Screen;Z)V", at = @At(value = "FIELD", target = "Lnet/minecraft/client/MinecraftClient;integratedServerRunning:Z", opcode = Opcodes.PUTFIELD))
     private void disconnect(CallbackInfo ci) {
-        System.out.println("disconnect");
+        MinecraftClientLifecycle.onExitWorld();
     }
 
     @Inject(method = "stop", at = @At("HEAD"))
     private void stop(CallbackInfo ci) {
-        System.out.println("stop");
+        MinecraftClientLifecycle.onExitGame();
     }
 }
