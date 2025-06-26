4d72b27efb558cf8224f0f1e7ae650f3544dde2c
nia <mangrove0720@outlook.com>
2025-06-27 07:48:48 +0800

    add dep bamboo-lib

---
+++ mods/bamboo-camera/src/main/java/bamboo/camera/ClientMain.java
@@ -0,0 +1,8 @@
+package bamboo.camera;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ClientMain implements ClientModInitializer {
+    public void onInitializeClient() {
+    }
+}
