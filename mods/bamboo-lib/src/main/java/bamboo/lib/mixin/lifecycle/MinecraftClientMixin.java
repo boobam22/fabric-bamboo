9f0b2986368f55f89997ad53161ec790e9e29ac7
nia <mangrove0720@outlook.com>
2025-06-27 07:10:08 +0800

    inject at joinWorld/exitWorld/exitGame

---
+++ mods/bamboo-lib/src/main/java/bamboo/lib/mixin/lifecycle/MinecraftClientMixin.java
@@ -0,0 +1,28 @@
+package bamboo.lib.mixin.lifecycle;
+
+import org.objectweb.asm.Opcodes;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.MinecraftClient;
+
+@Mixin(MinecraftClient.class)
+public abstract class MinecraftClientMixin {
+    @Inject(method = "joinWorld", at = @At("TAIL"))
+    private void joinWorld(CallbackInfo ci) {
+        System.out.println("joinWorld");
+    }
+
+    @Inject(method = "disconnect(Lnet/minecraft/client/gui/screen/Screen;Z)V", at = @At(value = "FIELD", target = "Lnet/minecraft/client/MinecraftClient;integratedServerRunning:Z", opcode = Opcodes.PUTFIELD))
+    private void disconnect(CallbackInfo ci) {
+        System.out.println("disconnect");
+    }
+
+    @Inject(method = "stop", at = @At("HEAD"))
+    private void stop(CallbackInfo ci) {
+        System.out.println("stop");
+    }
+}
