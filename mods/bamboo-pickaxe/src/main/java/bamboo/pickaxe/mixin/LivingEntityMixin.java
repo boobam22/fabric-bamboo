814f9a998edbc6ce3c6ab36d4b015c71ce529cc4
nia <mangrove0720@outlook.com>
2025-06-28 11:00:34 +0800

    drop spawn egg when kill by pickaxe

---
+++ mods/bamboo-pickaxe/src/main/java/bamboo/pickaxe/mixin/LivingEntityMixin.java
@@ -0,0 +1,27 @@
+package bamboo.pickaxe.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.boss.dragon.EnderDragonEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.registry.tag.ItemTags;
+
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin {
+    @Inject(method = "dropLoot", at = @At("HEAD"), cancellable = true)
+    private void dropLoot(ServerWorld world, DamageSource damageSource, boolean causedByPlayer, CallbackInfo ci) {
+        if (causedByPlayer && ((PlayerEntity) damageSource.getSource()).getMainHandStack().isIn(ItemTags.PICKAXES)) {
+            LivingEntity self = (LivingEntity) (Object) this;
+            if (!(self instanceof EnderDragonEntity)) {
+                self.dropStack(world, self.getPickBlockStack());
+                ci.cancel();
+            }
+        }
+    }
+}
