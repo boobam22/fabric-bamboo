1b2acf84cb57d8e54e10752dd0b9b9b742d58927
nia <mangrove0720@outlook.com>
2025-06-30 01:52:53 +0800

    support shulker box and crafting table

--- mods/bamboo-inventory/src/main/java/bamboo/inventory/mixin/ScreenHandlerMixin.java
+++ mods/bamboo-inventory/src/main/java/bamboo/inventory/mixin/ScreenHandlerMixin.java
@@ -6,32 +6,19 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.screen.ScreenHandler;
-import net.minecraft.screen.SimpleNamedScreenHandlerFactory;
-import net.minecraft.screen.ShulkerBoxScreenHandler;
-import net.minecraft.screen.GenericContainerScreenHandler;
 import net.minecraft.screen.slot.Slot;
-import net.minecraft.item.Items;
 import net.minecraft.item.ItemStack;
-import net.minecraft.registry.tag.ItemTags;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.inventory.Inventory;
 import net.minecraft.util.ClickType;
+
+import bamboo.inventory.action.OpenAction;
 
 @Mixin(ScreenHandler.class)
 public abstract class ScreenHandlerMixin {
     @Inject(method = "handleSlotClick", at = @At("HEAD"), cancellable = true)
     private void handleSlotClick(PlayerEntity player, ClickType clickType, Slot slot, ItemStack stack,
             ItemStack cursorStack, CallbackInfoReturnable<Boolean> cir) {
-        if (clickType == ClickType.LEFT) {
-            return;
-        }
-
-        if (stack.isIn(ItemTags.SHULKER_BOXES)) {
-        } else if (stack.getItem() == Items.ENDER_CHEST) {
-            Inventory inventory = player.getEnderChestInventory();
-            player.openHandledScreen(new SimpleNamedScreenHandlerFactory((syncId, playerInventory, serverPlayer) -> {
-                return GenericContainerScreenHandler.createGeneric9x3(syncId, playerInventory, inventory);
-            }, stack.getName()));
+        if (clickType == ClickType.RIGHT && OpenAction.open(player, stack, cursorStack)) {
             cir.setReturnValue(true);
         }
     }
