8af9f1c8135742ca45a6eb5cb3f71fa2fb8144f8
nia <mangrove0720@outlook.com>
2025-07-01 18:37:27 +0800

    reset to hit block

--- mods/bamboo-pickaxe/src/main/java/bamboo/pickaxe/AreaMine.java
+++ mods/bamboo-pickaxe/src/main/java/bamboo/pickaxe/AreaMine.java
@@ -5,6 +5,8 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.hit.BlockHitResult;
 
 public class AreaMine {
     private boolean enabled;
@@ -36,7 +38,12 @@ public class AreaMine {
     }
 
     public void resetArea() {
-        area = null;
+        HitResult hit = MinecraftClient.getInstance().crosshairTarget;
+        if (hit != null && hit.getType() == HitResult.Type.BLOCK) {
+            area = new Box(((BlockHitResult) hit).getBlockPos());
+        } else {
+            area = null;
+        }
     }
 
     public void mine(BlockPos pos) {
