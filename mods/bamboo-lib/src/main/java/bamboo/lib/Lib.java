7af4c431833e198a149b56a8a109bfe5d122d9c9
nia <mangrove0720@outlook.com>
2025-07-02 05:41:17 +0800

    add getter

--- mods/bamboo-lib/src/main/java/bamboo/lib/Lib.java
+++ mods/bamboo-lib/src/main/java/bamboo/lib/Lib.java
@@ -6,10 +6,18 @@ import bamboo.lib.config.ConfigRegistry;
 import bamboo.lib.command.Command;
 
 public class Lib implements ModInitializer {
-    public static final ConfigRegistry ConfigRegistry = new ConfigRegistry();
+    public static final ConfigRegistry configRegistry = new ConfigRegistry();
     public static final Registry<Command> commandRegistry = new Registry<>();
 
     @Override
     public void onInitialize() {
+    }
+
+    public ConfigRegistry getConfigRegistry() {
+        return configRegistry;
+    }
+
+    public Registry<Command> getCommandRegistry() {
+        return commandRegistry;
     }
 }
