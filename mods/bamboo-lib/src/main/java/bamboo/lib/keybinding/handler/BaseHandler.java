29cd359fd184d3e9b96ea4af3b8a9d400e3aeb35
nia <mangrove0720@outlook.com>
2025-07-02 06:21:29 +0800

    handler

--- mods/bamboo-lib/src/main/java/bamboo/lib/keybinding/handler/BaseHandler.java
+++ mods/bamboo-lib/src/main/java/bamboo/lib/keybinding/handler/BaseHandler.java
@@ -1,8 +1,8 @@
-package bamboo.lib.keybinding;
+package bamboo.lib.keybinding.handler;
 
 import net.minecraft.client.MinecraftClient;
 
 @FunctionalInterface
-public interface Handler {
+public interface BaseHandler {
     boolean apply(MinecraftClient client);
 }
