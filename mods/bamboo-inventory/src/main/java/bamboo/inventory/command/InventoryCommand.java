45a946431318a9ebde22a9a518ffc6399e744db7
nia <mangrove0720@outlook.com>
2025-07-03 01:36:58 +0800

    refesh trade

--- mods/bamboo-inventory/src/main/java/bamboo/inventory/command/InventoryCommand.java
+++ mods/bamboo-inventory/src/main/java/bamboo/inventory/command/InventoryCommand.java
@@ -1,5 +1,7 @@
 package bamboo.inventory.command;
 
+import java.util.List;
+
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
@@ -9,9 +11,14 @@ import com.mojang.brigadier.suggestion.SuggestionProvider;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.CommandManager.RegistrationEnvironment;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.command.CommandSource;
 import net.minecraft.command.CommandRegistryAccess;
 import net.minecraft.command.argument.BlockPosArgumentType;
 import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.entity.passive.VillagerEntity;
+import net.minecraft.village.VillagerProfession;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
 
 public class InventoryCommand implements bamboo.lib.command.Command {
     @Override
@@ -52,6 +59,11 @@ public class InventoryCommand implements bamboo.lib.command.Command {
         return 0;
     };
     private static Command<ServerCommandSource> refreshTrade = context -> {
+        VillagerEntity villager = (VillagerEntity) EntityArgumentType.getEntity(context, "villager");
+        if (villager.getExperience() == 0
+                && !villager.getVillagerData().profession().matchesKey(VillagerProfession.NONE)) {
+            villager.setOffers(null);
+        }
         return 0;
     };
 
@@ -62,6 +74,14 @@ public class InventoryCommand implements bamboo.lib.command.Command {
         return builder.buildFuture();
     };
     private static SuggestionProvider<ServerCommandSource> findVillager = (context, builder) -> {
-        return builder.buildFuture();
+        Vec3d pos = context.getSource().getPlayer().getPos();
+        Box range = new Box(pos, pos).expand(16);
+        List<String> villagers = context.getSource().getWorld()
+                .getEntitiesByClass(VillagerEntity.class, range, villager -> {
+                    return !villager.getVillagerData().profession().matchesKey(VillagerProfession.NONE)
+                            && villager.getExperience() == 0;
+                }).stream().map(e -> e.getUuidAsString()).toList();
+
+        return CommandSource.suggestMatching(villagers, builder);
     };
 }
