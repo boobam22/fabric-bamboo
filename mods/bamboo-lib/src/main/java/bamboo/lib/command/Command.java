3813b97cd9d1918db4bee55cbc6bc6596edd7d3d
nia <mangrove0720@outlook.com>
2025-07-04 00:32:18 +0800

    alias

--- mods/bamboo-lib/src/main/java/bamboo/lib/command/Command.java
+++ mods/bamboo-lib/src/main/java/bamboo/lib/command/Command.java
@@ -1,7 +1,11 @@
 package bamboo.lib.command;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 
+import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.CommandManager.RegistrationEnvironment;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.command.CommandRegistryAccess;
@@ -12,4 +16,12 @@ public interface Command {
             CommandDispatcher<ServerCommandSource> dispatcher,
             RegistrationEnvironment env,
             CommandRegistryAccess access);
+
+    default LiteralArgumentBuilder<ServerCommandSource> literal(String literal) {
+        return CommandManager.literal(literal);
+    }
+
+    default <T> RequiredArgumentBuilder<ServerCommandSource, T> argument(String name, ArgumentType<T> type) {
+        return CommandManager.argument(name, type);
+    }
 }
