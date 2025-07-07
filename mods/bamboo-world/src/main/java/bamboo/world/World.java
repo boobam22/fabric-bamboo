c291a4ed0c438dee8de0de484cc3bad6eae86aa6
nia <mangrove0720@outlook.com>
2025-07-07 19:25:07 +0800

    point command

--- mods/bamboo-world/src/main/java/bamboo/world/World.java
+++ mods/bamboo-world/src/main/java/bamboo/world/World.java
@@ -3,6 +3,7 @@ package bamboo.world;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.TreeMap;
 
 import net.fabricmc.api.ModInitializer;
 
@@ -11,9 +12,11 @@ import net.minecraft.registry.RegistryKey;
 import bamboo.lib.api.Server;
 import bamboo.world.command.WorldCommand;
 import bamboo.world.util.RegionPos;
+import bamboo.world.util.Point;
 
 public class World implements ModInitializer {
     public static Map<RegistryKey<net.minecraft.world.World>, List<RegionPos>> regions = new HashMap<>();
+    public static Map<String, Point> points = new TreeMap<>();
 
     @Override
     public void onInitialize() {
