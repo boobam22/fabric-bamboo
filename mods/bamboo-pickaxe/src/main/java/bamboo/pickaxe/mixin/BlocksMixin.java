7d4da9e7183fa50145dd7b6880137be5deac638b
nia <mangrove0720@outlook.com>
2025-09-02 05:26:10 +0800

    allow to break end_portal_frame

--- mods/bamboo-pickaxe/src/main/java/bamboo/pickaxe/mixin/BlocksMixin.java
+++ mods/bamboo-pickaxe/src/main/java/bamboo/pickaxe/mixin/BlocksMixin.java
@@ -1,5 +1,7 @@
 package bamboo.pickaxe.mixin;
 
+import java.util.function.Function;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,9 +13,13 @@ import net.minecraft.block.AbstractBlock.Settings;
 
 @Mixin(Blocks.class)
 public abstract class BlocksMixin {
-    @Inject(method = "register(Ljava/lang/String;Lnet/minecraft/block/AbstractBlock$Settings;)Lnet/minecraft/block/Block;", at = @At("HEAD"))
-    private static void register(String id, Settings settings, CallbackInfoReturnable<Block> cir) {
+    @Inject(method = "register(Ljava/lang/String;Ljava/util/function/Function;Lnet/minecraft/block/AbstractBlock$Settings;)Lnet/minecraft/block/Block;", at = @At("HEAD"))
+    private static void register(String id, Function<Settings, Block> factory, Settings settings,
+            CallbackInfoReturnable<Block> cir) {
         if (id.equals("bedrock")) {
+            settings.requiresTool().hardness(50);
+        }
+        if (id.equals("end_portal_frame")) {
             settings.requiresTool().hardness(50);
         }
     }
