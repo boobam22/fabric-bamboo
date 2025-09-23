a2b3e3335a6357107af77371f832db763682937e
nia <mangrove0720@outlook.com>
2025-09-23 20:35:46 +0800

    add ConfigEntry.presets

--- mods/bamboo-lib/src/main/java/bamboo/lib/config/ConfigEntry.java
+++ mods/bamboo-lib/src/main/java/bamboo/lib/config/ConfigEntry.java
@@ -1,5 +1,7 @@
 package bamboo.lib.config;
 
+import java.util.List;
+import java.util.ArrayList;
 import java.util.function.Function;
 
 public class ConfigEntry<T> {
@@ -7,12 +9,29 @@ public class ConfigEntry<T> {
     private final T defaultValue;
     private final Function<String, T> constructor;
     private T value;
+    private List<T> presets;
 
+    @SuppressWarnings("unchecked")
     public ConfigEntry(String key, T value, Function<String, T> constructor) {
         this.key = key;
         this.defaultValue = value;
         this.constructor = constructor;
         this.value = value;
+        this.presets = new ArrayList<>();
+
+        List<T> list;
+        if (value instanceof Boolean) {
+            list = (List<T>) List.of(true, false);
+        } else if (value instanceof Enum<?>) {
+            Class<? extends Enum<?>> enumClass = ((Enum<?>) value).getDeclaringClass();
+            list = (List<T>) List.of(enumClass.getEnumConstants());
+        } else {
+            return;
+        }
+
+        int idx = list.indexOf(defaultValue);
+        this.presets.addAll(list.subList(idx, list.size()));
+        this.presets.addAll(list.subList(0, idx));
     }
 
     public String getKey() {
@@ -27,12 +46,23 @@ public class ConfigEntry<T> {
         return value;
     }
 
+    public List<T> getPresets() {
+        return presets;
+    }
+
     public void set(T value) {
         this.value = value;
     }
 
     public void reset() {
         this.set(defaultValue);
+    }
+
+    public void toggle() {
+        if (presets.size() > 1) {
+            int idx = presets.indexOf(value);
+            this.set(presets.get((idx + 1) % presets.size()));
+        }
     }
 
     public void parse(String string) {
