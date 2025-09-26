163f4c90c9565a39aec82ef72e41a18ec09fd688
nia <mangrove0720@outlook.com>
2025-09-26 13:29:51 +0800

    refactor

--- mods/bamboo-render/src/main/java/bamboo/render/mixin/MinecraftClientMixin.java
+++ mods/bamboo-render/src/main/java/bamboo/render/mixin/MinecraftClientMixin.java
@@ -5,16 +5,16 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
 
 import bamboo.render.Render;
 
-@Mixin(Entity.class)
-public abstract class EntityMixin {
-    @Inject(method = "isGlowing", at = @At("HEAD"), cancellable = true)
-    private void isGlowing(CallbackInfoReturnable<Boolean> cir) {
-        Entity self = (Entity) (Object) this;
-        if (self.getWorld().isClient() && self.getType().isIn(Render.HIGHLIGHT_RENDER)) {
+@Mixin(MinecraftClient.class)
+public abstract class MinecraftClientMixin {
+    @Inject(method = "hasOutline", at = @At("HEAD"), cancellable = true)
+    private void hasOutline(Entity entity, CallbackInfoReturnable<Boolean> cir) {
+        if (entity.getType().isIn(Render.HIGHLIGHT_RENDER)) {
             cir.setReturnValue(true);
         }
     }
