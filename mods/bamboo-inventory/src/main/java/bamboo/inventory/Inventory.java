75e114eb712f08fe004df9bd1d14a1f49d889b55
nia <mangrove0720@outlook.com>
2025-10-02 16:15:57 +0800

    disable trade lock

--- mods/bamboo-inventory/src/main/java/bamboo/inventory/Inventory.java
+++ mods/bamboo-inventory/src/main/java/bamboo/inventory/Inventory.java
@@ -7,6 +7,7 @@ import bamboo.lib.config.ConfigEntry;
 
 public class Inventory implements ModInitializer {
     public static ConfigEntry<Boolean> villagerMaxBooks = Server.registerConfig("inventory.villagerMaxBooks", false);
+    public static ConfigEntry<Boolean> disableTradeLock = Server.registerConfig("inventory.disableTradeLock", false);
 
     @Override
     public void onInitialize() {
---
+++ mods/bamboo-inventory/src/main/java/bamboo/inventory/mixin/TradeOfferMixin.java
@@ -0,0 +1,20 @@
+package bamboo.inventory.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.village.TradeOffer;
+
+import bamboo.inventory.Inventory;
+
+@Mixin(TradeOffer.class)
+public abstract class TradeOfferMixin {
+    @Inject(method = "use", at = @At("HEAD"), cancellable = true)
+    private void use(CallbackInfo ci) {
+        if (Inventory.disableTradeLock.getValue()) {
+            ci.cancel();
+        }
+    }
+}
