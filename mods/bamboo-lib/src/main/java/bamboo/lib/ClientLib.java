0b8acb44e4c6ce6037ee1c0e60b90e1c8946f298
nia <mangrove0720@outlook.com>
2025-10-02 22:32:57 +0800

    dynamic keybind to send command

--- mods/bamboo-lib/src/main/java/bamboo/lib/ClientLib.java
+++ mods/bamboo-lib/src/main/java/bamboo/lib/ClientLib.java
@@ -4,10 +4,12 @@ import net.fabricmc.api.ClientModInitializer;
 
 import bamboo.lib.api.Client;
 import bamboo.lib.config.ConfigRegistry;
+import bamboo.lib.config.ConfigEntry;
 import bamboo.lib.keybinding.IngameHandler;
 
 public class ClientLib implements ClientModInitializer {
     public static final ConfigRegistry configRegistry = new ConfigRegistry("bamboo.properties");
+    public static final ConfigEntry<CommandKeybind> commandKeybinds;
     public static final Registry<Runnable> joinWorldHandlers = new Registry<>();
     public static final Registry<Runnable> exitWorldHandlers = new Registry<>();
     public static final Registry<Runnable> exitGameHandlers = new Registry<>();
@@ -25,4 +27,13 @@ public class ClientLib implements ClientModInitializer {
         Client.message("Config reloaded");
         return true;
     };
+
+    static {
+        CommandKeybind ck = new CommandKeybind();
+        ck.put("t+f", "tick freeze");
+        ck.put("t+r", "tick unfreeze");
+        ck.put("t+1", "tick step 1t");
+        ck.put("t+2", "tick step 2t");
+        commandKeybinds = Client.registerConfig("lib.commandKeybinds", ck, CommandKeybind::from);
+    }
 }
