29943fa91a7c2875db55888d72571c4be80e04ec
nia <mangrove0720@outlook.com>
2025-10-02 16:04:49 +0800

    refresh villager trade offer of each level

--- mods/bamboo-inventory/src/main/java/bamboo/inventory/screen/Util.java
+++ mods/bamboo-inventory/src/main/java/bamboo/inventory/screen/Util.java
@@ -1,5 +1,7 @@
 package bamboo.inventory.screen;
 
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.screen.ScreenHandlerFactory;
 import net.minecraft.screen.GenericContainerScreenHandler;
@@ -14,9 +16,15 @@ import net.minecraft.component.type.ContainerComponent;
 import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.village.TradeOfferList;
+import net.minecraft.village.TradeOffers;
+import net.minecraft.village.VillagerData;
 import net.minecraft.village.VillagerProfession;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.text.Text;
-
+import net.minecraft.resource.featuretoggle.FeatureFlags;
+import bamboo.inventory.mixin.MerchantEntityAccessor;
 import bamboo.inventory.mixin.MerchantScreenHandlerAccessor;
 
 public class Util {
@@ -77,11 +85,28 @@ public class Util {
     public static void refreshTrade(ServerPlayerEntity player) {
         VillagerEntity villager = (VillagerEntity) ((MerchantScreenHandlerAccessor) player.currentScreenHandler)
                 .bamboo_getMerchant();
-        if (villager.getExperience() == 0
-                && !villager.getVillagerData().profession().matchesKey(VillagerProfession.NONE)) {
-            villager.setOffers(null);
-            player.sendTradeOffers(player.currentScreenHandler.syncId, villager.getOffers(),
-                    villager.getVillagerData().level(), villager.getExperience(),
+        int exp = villager.getExperience();
+        VillagerData villagerData = villager.getVillagerData();
+        int level = villagerData.level();
+        RegistryEntry<VillagerProfession> profession = villagerData.profession();
+
+        if ((exp == VillagerData.getUpperLevelExperience(level - 1))
+                && !profession.matchesKey(VillagerProfession.NONE)) {
+            RegistryKey<VillagerProfession> professionKey = profession.getKey().orElse(VillagerProfession.NONE);
+
+            Int2ObjectMap<TradeOffers.Factory[]> factory = null;
+            if (villager.getWorld().getEnabledFeatures().contains(FeatureFlags.TRADE_REBALANCE)) {
+                factory = TradeOffers.REBALANCED_PROFESSION_TO_LEVELED_TRADE.get(professionKey);
+            }
+            if (factory == null) {
+                factory = TradeOffers.PROFESSION_TO_LEVELED_TRADE.get(professionKey);
+            }
+            TradeOffers.Factory[] pool = factory.get(level);
+
+            TradeOfferList offers = villager.getOffers();
+            offers.subList(offers.size() - Math.min(2, pool.length), offers.size()).clear();
+            ((MerchantEntityAccessor) villager).bamboo_fillRecipesFromPool(offers, pool, 2);
+            player.sendTradeOffers(player.currentScreenHandler.syncId, offers, level, exp,
                     villager.isLeveledMerchant(), villager.canRefreshTrades());
         }
     }
